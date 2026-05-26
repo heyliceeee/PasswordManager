1b3846c94c57567ced9a99e05610b7fa953d3a40
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-26 16:49:29 +0100

    💄feat: create labels | create btns | create textbox

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ FONT_NAME = "Courier"
 IMAGE = "/data/logo.png"
 dir_path = os.path.dirname(os.path.abspath(__file__)) # get the path of the current file
 
-window = Tk() # create a window
-
-# password generator
-
-
-
-# save password
-
-
+def generate_password():
+    """
+    generate a random password
+    """
+    pass
+def add():
+    """
+    add a new password
+    """
+    pass
 
 
 # UI setup
@@ -23,7 +24,19 @@ def create_window():
     create a window, set a title, set the size, and set the padding
     """
     window.title("Password Manager") # set the title of the window
-    window.config(padx=20, pady=20)  # set the padding of the window
+    window.config(padx=50, pady=50)  # set the padding of the window
+def create_labels():
+    """
+    create the labels, set the text, and place them on the screen
+    """
+    website_label = Label(text="Website ") # create a label
+    website_label.grid(column=0, row=1) # place the label on the screen
+
+    email_username_label = Label(text="Email/Username ") # create a label
+    email_username_label.grid(column=0, row=2) # place the label on the screen
+
+    password_label = Label(text="Password ") # create a label
+    password_label.grid(column=0, row=3) # place the label on the screen
 def create_canvas():
     """
     create a canvas
@@ -34,8 +47,34 @@ def create_canvas():
     canvas = Canvas(width=200, height=200, highlightthickness=0) # create a canvas
     canvas.img = img # assign the image to a variable
     canvas.create_image(100, 100, image=img) # place the image
-    canvas.grid(column=1, row=1)  # place the canvas
+    canvas.grid(column=1, row=0)  # place the canvas
+def create_btns():
+    """
+    create the buttons, set the text, and place them on the window
+    """
+    generate_password_btn = Button(text="Generate Password", command=generate_password, highlightthickness=0) # create the button
+    generate_password_btn.grid(column=2, row=3) # place the button on the window
 
+    add_btn = Button(width=36, text="Add", command=add, highlightthickness=0) # create the button
+    add_btn.grid(column=1, row=4, columnspan=2) # place the button on the window
+def create_textbox():
+    """
+    create a text box, set the text, and place it on the screen
+    """
+    input_website.grid(column=1, row=1, columnspan=2)
+    input_email_username.grid(column=1, row=2, columnspan=2)
+    input_password.grid(column=1, row=3)
+
+window = Tk() # create a window
 create_window() # call the function to create the window
+
+create_labels() # call the function to create the labels
 create_canvas() # call the function to create the canvas
+create_btns() # call the function to create the buttons
+
+input_website = Entry(width=38) # create a text box
+input_email_username = Entry(width=38) # create a text box
+input_password = Entry(width=21) # create a text box
+create_textbox() # call the function to create the textbox
+
 window.mainloop() # continuously run the program
